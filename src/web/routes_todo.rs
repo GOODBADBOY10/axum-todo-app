71b130a33034e31c957c73d9d849504cef791067
Goodbadboy <selfmanademola@gmail.com>
2025-10-19 01:51:22 +0100

    fix: integrated axum statuscode logic for error

--- src/web/routes_todo.rs
+++ src/web/routes_todo.rs
@@ -1,10 +1,16 @@
-use axum::{routing::post, Router};
+use axum::{
+    routing::post,
+    Router,
+    response::IntoResponse,
+    http::StatusCode,
+};
 
 pub fn routes() -> Router {
     Router::new()
-    .route("/api/todo",
-    post(api_todo))
+        .route("/api/todo", post(api_todo))
 }
 
-
-async fn api_todo() {}
+async fn api_todo() -> impl IntoResponse {
+    println!("✅ TODO endpoint hit");
+    (StatusCode::OK, "Todo endpoint reached")
+}
